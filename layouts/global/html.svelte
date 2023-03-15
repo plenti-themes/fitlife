e575b7debe79e7b09ad84bd3f0067d6549215da0
jimafisk <jimafisk@gmail.com>
2023-03-14 22:03:29 -0400

    Add CMS.

--- layouts/global/html.svelte
+++ layouts/global/html.svelte
@@ -1,8 +1,17 @@
 <script>
+  import { onMount } from 'svelte';
   import Footer from './footer.svelte';
   import Head from './head.svelte';
   import Nav from "./nav.svelte";
-  export let content, layout, allContent, allLayouts;
+  import Login from "./login.svelte";
+
+  export let content, layout, allContent, allLayouts, user, adminMenu;
+  
+  let hash;
+  onMount(async () => {
+    hash = window.location.hash;
+  });
+
   let y;
 </script>
 
@@ -11,18 +20,24 @@
 <html lang="en">
   <Head title={content.fields?.meta?.title} />
   <body>
-    <a href="#top" class="back-top-btn{y > 100 ? ' active' : ''}" aria-label="back to top" data-back-top-btn="">
-      <ion-icon name="caret-up-sharp" aria-hidden="true" role="img" class="md hydrated"></ion-icon>
-    </a>
-    <header class="header{y > 100 ? ' active' : ''}" data-header="">
-      <div class="container">
-          <Nav />
-      </div>
-    </header>
-    <svelte:component this={layout} {...content.fields} {allContent} {allLayouts} {content}/>
-    <Footer />
-    <a href="#top" class="back-top-btn" aria-label="back to top" data-back-top-btn>
-      <ion-icon name="caret-up-sharp" aria-hidden="true"></ion-icon>
-    </a>
+    {#if user && $user.isAuthenticated}
+      <svelte:component this={adminMenu} {user} bind:content={content} />
+    {/if}
+    <Login bind:hash {user} />
+    <main>
+      <a href="#top" class="back-top-btn{y > 100 ? ' active' : ''}" aria-label="back to top" data-back-top-btn="">
+        <ion-icon name="caret-up-sharp" aria-hidden="true" role="img" class="md hydrated"></ion-icon>
+      </a>
+      <header class="header{y > 100 ? ' active' : ''}" data-header="">
+        <div class="container">
+            <Nav />
+        </div>
+      </header>
+      <svelte:component this={layout} {...content.fields} {allContent} {allLayouts} {content}/>
+      <Footer />
+      <a href="#top" class="back-top-btn" aria-label="back to top" data-back-top-btn>
+        <ion-icon name="caret-up-sharp" aria-hidden="true"></ion-icon>
+      </a>
+    </main>
   </body>
 </html>
