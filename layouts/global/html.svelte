8a8be58669b0f31d92b1f45d079ab65262b37299
jimafisk <jimafisk@gmail.com>
2023-03-14 22:15:58 -0400

    Move Nav wrapper.

--- layouts/global/html.svelte
+++ layouts/global/html.svelte
@@ -11,11 +11,7 @@
   onMount(async () => {
     hash = window.location.hash;
   });
-
-  let y;
 </script>
-
-<svelte:window bind:scrollY={y}/>
 
 <html lang="en">
   <Head title={content.fields?.meta?.title} />
@@ -25,14 +21,7 @@
     {/if}
     <Login bind:hash {user} />
     <main>
-      <a href="#top" class="back-top-btn{y > 100 ? ' active' : ''}" aria-label="back to top" data-back-top-btn="">
-        <ion-icon name="caret-up-sharp" aria-hidden="true" role="img" class="md hydrated"></ion-icon>
-      </a>
-      <header class="header{y > 100 ? ' active' : ''}" data-header="">
-        <div class="container">
-            <Nav />
-        </div>
-      </header>
+      <Nav />
       <svelte:component this={layout} {...content.fields} {allContent} {allLayouts} {content}/>
       <Footer />
       <a href="#top" class="back-top-btn" aria-label="back to top" data-back-top-btn>
--- layouts/global/nav.svelte
+++ layouts/global/nav.svelte
@@ -1,56 +1,54 @@
+<script>
+  let y, navActive;
 
-      <script>
-        let y, navActive;
-          const toggleMenu = () => {
-          navActive = !navActive;
-          }
-          const scroll = e => {
-               document.getElementById(e.target.hash.substring(1)).scrollIntoView();
-          }
-      </script>
-     <svelte:window bind:scrollY={y}/>
-      <a href="." class="logo">
-        <ion-icon name="barbell-sharp" aria-hidden="true" role="img" class="md hydrated"></ion-icon>
+  const toggleMenu = () => {
+    navActive = !navActive;
+  }
 
-        <span class="span">Fitlife</span>
-      </a>
-     
-      <nav class="navbar {navActive ? ' active' : ''} {y > 20 ? ' sticky' : ''}" on:click={toggleMenu} data-navbar="">
+  const scroll = e => {
+    document.getElementById(e.target.hash.substring(1)).scrollIntoView();
+  }
+</script>
 
-        <button class="nav-close-btn" aria-label="close menu" data-nav-toggler="">
-          <ion-icon name="close-sharp" aria-hidden="true" role="img" class="md hydrated"></ion-icon>
-        </button>
+<svelte:window bind:scrollY={y}/>
 
-        <ul class="navbar-list">
-
-          <li>
-            <a on:click|preventDefault={e => scroll(e)} href="#home" class="navbar-link" data-nav-link="">Home</a>
-          </li>
-
-          <li>
-            <a on:click|preventDefault={e => scroll(e)} href="#about" class="navbar-link" data-nav-link="">About Us</a>
-          </li>
-
-          <li>
-            <a on:click|preventDefault={e => scroll(e)} href="#class" class="navbar-link" data-nav-link="">Classes</a>
-          </li>
-
-          <li>
-            <a on:click|preventDefault={e => scroll(e)} href="#blog" class="navbar-link" data-nav-link="">Blog</a>
-          </li>
-
-          <li>
-            <a on:click|preventDefault={e => scroll(e)} href="." class="navbar-link" data-nav-link="">Contact Us</a>
-          </li>
-
-        </ul>
-
-      </nav>
-
-      <a href="." class="btn btn-secondary">Join Now</a>
-
-      <button class="nav-open-btn" on:click={toggleMenu} aria-label="open menu" data-nav-toggler="{navActive ? ' active' : ''}">
-        <span class="line"></span>
-        <span class="line"></span>
-        <span class="line"></span>
+<header class="header{y > 100 ? ' active' : ''}" data-header="">
+  <div class="container">
+    <a href="." class="logo">
+      <ion-icon name="barbell-sharp" aria-hidden="true" role="img" class="md hydrated"></ion-icon>
+      <span class="span">Fitlife</span>
+    </a>
+    <nav class="navbar {navActive ? ' active' : ''} {y > 20 ? ' sticky' : ''}" on:click={toggleMenu} data-navbar="">
+      <button class="nav-close-btn" aria-label="close menu" data-nav-toggler="">
+        <ion-icon name="close-sharp" aria-hidden="true" role="img" class="md hydrated"></ion-icon>
       </button>
+      <ul class="navbar-list">
+        <li>
+          <a on:click|preventDefault={e => scroll(e)} href="#home" class="navbar-link" data-nav-link="">Home</a>
+        </li>
+        <li>
+          <a on:click|preventDefault={e => scroll(e)} href="#about" class="navbar-link" data-nav-link="">About Us</a>
+        </li>
+        <li>
+          <a on:click|preventDefault={e => scroll(e)} href="#class" class="navbar-link" data-nav-link="">Classes</a>
+        </li>
+        <li>
+          <a on:click|preventDefault={e => scroll(e)} href="#blog" class="navbar-link" data-nav-link="">Blog</a>
+        </li>
+        <li>
+          <a on:click|preventDefault={e => scroll(e)} href="." class="navbar-link" data-nav-link="">Contact Us</a>
+        </li>
+      </ul>
+    </nav>
+    <a href="." class="btn btn-secondary">Join Now</a>
+    <button class="nav-open-btn" on:click={toggleMenu} aria-label="open menu" data-nav-toggler="{navActive ? ' active' : ''}">
+      <span class="line"></span>
+      <span class="line"></span>
+      <span class="line"></span>
+    </button>
+  </div>
+</header>
+
+<a href="#top" class="back-top-btn{y > 100 ? ' active' : ''}" aria-label="back to top" data-back-top-btn="">
+  <ion-icon name="caret-up-sharp" aria-hidden="true" role="img" class="md hydrated"></ion-icon>
+</a>
