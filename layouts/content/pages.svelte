6acef4768ce35e8d1e3193ebc788d70138ae1925
jimafisk <jimafisk@gmail.com>
2023-03-15 22:52:14 -0400

    Pages content type.

--- layouts/content/pages.svelte
+++ layouts/content/pages.svelte
@@ -1,11 +1,9 @@
 <script>
 	export let components, allLayouts;
 </script>
+
 {#if components}
 	{#each components as {name, fields}}
 		<svelte:component this={allLayouts["layouts_components_" + name + "_svelte"]} {...fields} />
 	{/each}
 {/if}
-<style>
-
-</style>
