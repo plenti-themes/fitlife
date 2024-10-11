28f265121f3c9762ba9114905eadc19d63280e43
Benjamin Waller <benjaminjameswaller@gmail.com>
2024-10-12 10:55:48 +1100

    update logo path to media folder

--- layouts/global/head.svelte
+++ layouts/global/head.svelte
@@ -12,7 +12,7 @@
   <script type="module" src="spa/core/main.js"></script>
   <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
   <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
-  <link rel="icon" type="image/svg+xml" href="assets/logo.svg">
+  <link rel="icon" type="image/svg+xml" href="media/logo.svg">
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="">
   <link rel="shortcut icon" href="favicon.svg" type="image/svg+xml">
